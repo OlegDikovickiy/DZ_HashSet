9878ad517e72d4d4fb3c84c22d7521c1515a1996
Oleg Dikovickiy <Dikovickiy526@inbox.ru>
2022-10-06 22:24:41 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+  private static final String TEXT = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
+
+  public static void main(String[] args) {
+
+    WordsChecker wordsChecker = new WordsChecker(TEXT);
+
+    String word = "consectetur";
+    String word2 = "sense";
+    System.out.println("В тексте есть слово: "+ word + ", ответ: " + wordsChecker.hasWord(word));
+    System.out.println("В тексте есть слово: "+ word2 + ", ответ: " + wordsChecker.hasWord(word2));
+  }
+}
